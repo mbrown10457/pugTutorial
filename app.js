faa67f8c9fc62b762edda1a774a16415376f45dd
mbrown10457 <mike@mikes-mac-pro.attlocal.net>
2017-03-21 14:10:35 -0700

    Mike - Commit#2
    Commit with CSS and additional HTML + cleanup HTML

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var app  = express();
 
 app.set('view engine', 'pug');
 
-app.get("/",function(req, res){
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
 	res.render("home.pug");
+});
+
+app.get("/birds", function(req, res){
+	res.render('birds.pug');
 });
 
 app.get("/bestever/:sport", function(req, res){
