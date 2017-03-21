d1c8a7427d392323bf656b502ac3bbaf6551853a
mbrown10457 <mike@mikes-mac-pro.attlocal.net>
2017-03-21 16:51:07 -0700

    Mike - CSS Styling

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require ("express");
+
 var app  = express();
 
 app.set('view engine', 'pug');
 
-app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static('public'));
 
 app.get("/", function(req, res){
 	res.render("home.pug");
@@ -18,6 +19,7 @@ app.get("/bestever/:sport", function(req, res){
 	res.render('sport.pug', {sportVar: sport});
 });
 
+// Open express on port 3000
 app.listen(3000, function(){
 	console.log("Port 3000 is now serving app.js");
 });
